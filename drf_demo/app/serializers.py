4f9efd89987e23fecc2417324606956921f411ba
m124578n <m23568n@gmail.com>
2023-12-13 09:30:01 +0800

    serializer customer field

--- drf_demo/app/serializers.py
+++ drf_demo/app/serializers.py
@@ -1,16 +1,22 @@
+from django.utils.timezone import now
 from rest_framework import serializers
 from app.models import Music
-from django.utils.timezone import now
+
+
+class ToUpperCaseCharField(serializers.CharField):
+    def to_representation(self, value):
+        return value.upper()
 
 
 class MusicSerializer(serializers.ModelSerializer):
-   days_since_created = serializers.SerializerMethodField()
+    days_since_created = serializers.SerializerMethodField()
+    singer = ToUpperCaseCharField()
 
-   class Meta:
-       model = Music
-       # fields = '__all__'
-       fields = ('id', 'song', 'singer', 'last_modify_date', 'created', 'days_since_created')
+    class Meta:
+        model = Music
+        # fields = '__all__'
+        fields = ('id', 'song', 'singer', 'last_modify_date', 'created', 'days_since_created')
 
-   def get_days_since_created(self, obj):
-       return (now() - obj.created).days
-   
+    def get_days_since_created(self, obj):
+        return (now() - obj.created).days
+    
