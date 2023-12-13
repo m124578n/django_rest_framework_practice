02c391035cb3e209c5d459d4fd03d134d0bec274
m124578n <m23568n@gmail.com>
2023-12-13 09:56:07 +0800

    add update data with raw sql

--- drf_demo/app/views.py
+++ drf_demo/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from app.models import Music, fun_raw_sql_query
+from app.models import Music, fun_raw_sql_query, fun_sql_cursor_update
 from app.serializers import MusicSerializer
 
 from rest_framework import viewsets, status
@@ -14,11 +14,19 @@ class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
 
+
     # /api/music/raw_sql_query/
-    @action(detail=False)
+    @action(methods=['get'], detail=False)
     def raw_sql_query(self, request):
         song = request.query_params.get('song', None)
         music = fun_raw_sql_query(song=song)
         serializer = MusicSerializer(music, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    # /api/music/{pk}/sql_cursor_update/
+    @action(methods=['put'], detail=True)
+    def sql_cursor_update(self, request, pk=None):
+        song = request.data.get('song', None)
+        if song:
+            music = fun_sql_cursor_update(song=song, pk=pk)
+            return Response(music, status=status.HTTP_200_OK)
