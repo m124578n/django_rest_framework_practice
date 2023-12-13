8c1381ec892c652e1f416f886d900cb3857e92a9
m124578n <m23568n@gmail.com>
2023-12-13 10:14:35 +0800

    create app shares and test for the permission

--- drf_demo/drf_demo/urls.py
+++ drf_demo/drf_demo/urls.py
@@ -17,13 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from app import views
+
+from app.views import MusicViewSet
+from shares.views import ShareViewSet
 
 router = DefaultRouter()
-router.register(r'music', views.MusicViewSet)
+router.register(r'music', MusicViewSet, basename='music')
+router.register(r'shares', ShareViewSet, basename='share')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls), name='api'),
     path('api-auth/', include('rest_framework.urls'), name='rest_framwork')
 ]
